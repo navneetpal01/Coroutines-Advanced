368f15adfe12ed799dcd86276e4d300197d3cb06
navneet <navneetpalgithub@gmail.com>
2024-09-17 17:48:22 +0530

    Coroutines-Advanced

--- app/src/main/java/com/app/coroutines_advanced/Coroutine_Context.kt
+++ app/src/main/java/com/app/coroutines_advanced/Coroutine_Context.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * If you look at coroutine builder there first parameter is of type CoroutineContext
@@ -46,14 +47,14 @@ fun main13() {
  * null.
  */
 
-fun main14(){
+fun main14() {
 
-    val ctx : CoroutineContext = CoroutineName("A Name")
+    val ctx: CoroutineContext = CoroutineName("A Name")
 
-    val coroutineName : CoroutineName? = ctx[CoroutineName]
+    val coroutineName: CoroutineName? = ctx[CoroutineName]
     // or ctx.get(CoroutineName)
     println(coroutineName?.name) // A name
-    val job : Job? = ctx[Job] // or ctx.get(Job)
+    val job: Job? = ctx[Job] // or ctx.get(Job)
     println(job) // null
 
     /**
@@ -77,6 +78,140 @@ fun main14(){
 interface Job : CoroutineContext.Element {
     companion object Key : CoroutineContext.Key<Job>
 }
+
+// Adding CoroutineContext
+
+fun main15() {
+
+    val ctx1: CoroutineContext = CoroutineName("Name1")
+    println(ctx1[CoroutineName]?.name) // Name1
+    println(ctx1[Job]?.isActive) // null
+
+    val ctx2: CoroutineContext = Job()
+    println(ctx2[CoroutineName]?.name) // null
+    println(ctx2[Job]?.isActive) // true, because "Active"
+    // is the default state of a job created this way
+
+    val ctx3 = ctx1 + ctx2
+    println(ctx3[CoroutineName]?.name) // Name1
+    println(ctx3[Job]?.isActive) // true
+
+}
+
+/**
+ * The real power of CoroutineContext lies in its ability to merge contexts.
+ * When combining contexts with different keys, the result responds to both keys.
+ * If a new element with an existing key is added, it replaces the previous element,
+ * similar to a map.
+ *
+ * The companion object here is named Key (instead of the default Companion),
+ * but this doesn't change how it's used, only its reference name in reflection or extension functions.
+ */
+
+fun main16() {
+
+    val ctx1: CoroutineContext = CoroutineName("Name1")
+    println(ctx1[CoroutineName]?.name) // Name1
+
+    val ctx2: CoroutineContext = CoroutineName("Name2")
+    println(ctx2[CoroutineName]?.name) // Name2
+
+    val ctx3 = ctx1 + ctx2
+    println(ctx3[CoroutineName]?.name) // Name2
+
+
+}
+
+// Empty CoroutineContext
+
+fun main17() {
+
+    val empty: CoroutineContext = EmptyCoroutineContext
+    println(empty[CoroutineName]) // null
+    println(empty[Job]) // null
+
+    val ctxName = empty + CoroutineName("Name1") + empty
+    println(ctxName[CoroutineName]) // CoroutineName(Name1)
+
+}
+
+/**
+ * Subtracting elements
+ * Elements can be removed from a context using minusKey.
+ * The minus operator isn't overloaded for CoroutineContext to avoid ambiguity,
+ * as explained in Effective Kotlin Item 12.
+ */
+
+fun main18() {
+
+    val ctx = CoroutineName("Name1") + Job()
+    println(ctx[CoroutineName]?.name) // Name1
+    println(ctx[Job]?.isActive) // true
+
+    val ctx2 = ctx.minusKey(CoroutineName)
+    println(ctx[CoroutineName]?.name) // null
+    println(ctx[Job]?.isActive) // true
+
+    val ctx3 = (ctx + CoroutineName("Name2"))
+        .minusKey(CoroutineName)
+    println(ctx3[CoroutineName]?.name) // null
+    println(ctx3[Job]?.isActive) // true
+
+}
+
+/**
+ * Folding context
+ * To perform an operation on each element within a context in Kotlin Coroutines, use the fold method. It requires:
+ *     An initial accumulator value
+ *     An operation to update the accumulator based on its current state and the current element
+ */
+
+fun main() {
+
+    val ctx = CoroutineName("Name1") + Job()
+
+    ctx.fold("") { acc, element -> "$acc$element" }
+        .also(::println)
+    // CoroutineName(Name1) JobImpl{Active}@dbab622e
+
+    val empty = emptyList<CoroutineContext>()
+    ctx.fold(empty) { acc, element -> acc + element }
+        .joinToString()
+        .also(::println)
+    // CoroutineName(Name1) , JobImpl{Active}@dbab622e
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
